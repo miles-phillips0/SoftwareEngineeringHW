58f2e628e1f37a00c19f74b6d70267550d49a1d0
root <root@DESKTOP-0J1LISS.localdomain>
2022-01-20 18:16:16 -0500

    finished the rest of the hw

--- hw3.py
+++ hw3.py
@@ -23,29 +23,64 @@ def VowelsVsConsonants(str):
     return None
 
 #question 2
-def cylinderVolume(radius, height):
+def CylinderVolume(radius, height):
     return (2 * radius * height * math.pi)
 
 #question 3
-def commaConcat(strList):
+def CommaConcat(strList):
     combined = ""
     for str in strList:
         combined += (str + ", ")
+    
     #this part just takes off the last comma
     combined = combined[0:len(combined)-2]
     return combined
 
 #question 4
-def concatFile(strLists):
+def ConcatFile(strLists):
     combined = ""
     f = open('output.txt' , 'w')
     for list in  strLists:
-        combined += (commaConcat(list) + ", ")
+        combined += (CommaConcat(list) + ", ")
+    
     combined = combined[0:len(combined)-2]
     f.write(combined)
     f.close()
     return os.path.abspath('output.txt')
 
-listList = [["this", "is"], ["a"], ["test", "!"]]
-concatFile(listList)
+#question 5
+def FileToList(filePath):
+    combined = []
+    currentLine = []
+    f = open(filePath , 'r')
+    for line in f.readlines():  
+        currentLine = line.split(", ")
+        combined.append(currentLine)
+    
+    return combined
+
+#question 6
+def divideTwo(numerator, denominator):
+    while True:
+        try:
+            divided = numerator / denominator
+            return divided
+        except ZeroDivisionError:
+            print("divide by 0 error")
+            denominator = int(input("Enter a new denominator: "))
+
+#question 7
+def noDuplicates(intList):
+    newList = []
+    for i in intList:
+        if i not in newList:
+            newList.append(i)
+    return newList
+
+#question 8
+def createDirectory():
+    path = os.getcwd() + "/hw3-folder"
+    os.mkdir(path)
+
+createDirectory()
 
