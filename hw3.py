048fc0a029b068a43d59439d1899cb227951ea46
root <root@DESKTOP-0J1LISS.localdomain>
2022-01-20 15:57:44 -0500

    completed Qs 2 through 4

--- hw3.py
+++ hw3.py
@@ -1,4 +1,8 @@
-#question One
+import math
+import os
+
+
+#question 1
 def VowelsVsConsonants(str):
     str = str.lower()
     vowles = "aeiouy"
@@ -18,6 +22,30 @@ def VowelsVsConsonants(str):
     
     return None
 
-print(VowelsVsConsonants("faaaaeeeeiii"))
+#question 2
+def cylinderVolume(radius, height):
+    return (2 * radius * height * math.pi)
 
+#question 3
+def commaConcat(strList):
+    combined = ""
+    for str in strList:
+        combined += (str + ", ")
+    #this part just takes off the last comma
+    combined = combined[0:len(combined)-2]
+    return combined
+
+#question 4
+def concatFile(strLists):
+    combined = ""
+    f = open('output.txt' , 'w')
+    for list in  strLists:
+        combined += (commaConcat(list) + ", ")
+    combined = combined[0:len(combined)-2]
+    f.write(combined)
+    f.close()
+    return os.path.abspath('output.txt')
+
+listList = [["this", "is"], ["a"], ["test", "!"]]
+concatFile(listList)
 
